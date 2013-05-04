af0894dfa4e9691cdab33f06685f145d660959b3
Chris Hanna <cdhanna@wpi.edu>
2013-05-04 01:22:22 -0400

    added some basic interfaces

---
+++ MagicGame/MagicGame/IAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicGame
+{
+    interface IAction
+    {
+        /// <summary>
+        /// fetches the cost of this action
+        /// </summary>
+        /// <returns>the cost of the action</returns>
+        public ICost getCost();
+
+        /// <summary>
+        /// fetches the type of this action
+        /// </summary>
+        /// <returns>the type of the action</returns>
+        public IActionType getType();
+
+        /// <summary>
+        /// run the action
+        /// </summary>
+        public void runEffects();
+
+    }
+}
---
+++ MagicGame/MagicGame/ICard.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicGame
+{
+    /// <summary>
+    /// The basic interface for a card.
+    /// </summary>
+    interface ICard
+    {
+        /// <summary>
+        /// fetches the name of the card
+        /// </summary>
+        /// <returns> the name of the card </returns>
+        public String getName();
+
+        /// <summary>
+        /// fetches the cost to play the card
+        /// </summary>
+        /// <returns> the cost of the card </returns>
+        public ICost getCost();
+
+        /// <summary>
+        /// fetches the type of the card
+        /// </summary>
+        /// <returns>the type of the card </returns>
+        public ICardType getType();
+
+        /// <summary>
+        /// fetches the list of actions the card has
+        /// </summary>
+        /// <returns>the list of actions for the card </returns>
+        public List<IAction> getActions();
+
+        /// <summary>
+        /// fetches the toughness/damage of the card if it is a creature. If it is not, this will return an EmptyCreatureStats
+        /// </summary>
+        /// <returns>the toughness/damage of the card</returns>
+        public ICreatureStats getCreatureStats();
+
+
+    }
+}
